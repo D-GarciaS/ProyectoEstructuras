f5305e065583c94701c26ec938b6094eba1fa24a
Damian-GS <Hogar@Hogar-PC>
2015-11-25 21:06:14 -0600

    Creación del Proyecto

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Hogar
+ *
+ * Created on 25 de noviembre de 2015, 09:01 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
